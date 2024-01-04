45d415fb00f2c035b571a5e3a9b08e41040022a3
Prashant Kumar <pb28.rkt@gmail.com>
2024-01-05 00:09:00 +0530

    05.TS_Objects

---
+++ 05.TypeScript/05.TS_Objects/objects.ts
@@ -0,0 +1,67 @@
+interface Customer {
+  name: string;
+  age: number;
+  occupation?: string; // ( ? == > Optional)
+}
+
+const customer: Customer = {
+  name: "Warns",
+  age: 34,
+};
+
+console.log(customer); //   { name: 'Warns', age: 34 }
+
+//   Nested Objects
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+const user: User = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
+};
+
+console.log(user);
