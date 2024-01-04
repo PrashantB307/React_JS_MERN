2ecfc7c3ea9e46037e715c8259f269757b8bb52e
Prashant Kumar <pb28.rkt@gmail.com>
2024-01-05 00:08:36 +0530

    03.TS_Operators

---
+++ 05.TypeScript/03.TS_Operators/operators.ts
@@ -0,0 +1,18 @@
+const n1 : number = 100;
+const n2 : number = 200;
+const res : number = n1 + n2;
+console.log(`result : ${res}`);   
+
+
+//    Conditional
+
+const marks : number = 78;
+let theResult : string = "";
+if(marks <= 35){
+    theResult = "You Failed";
+}else{
+    theResult = "You Passed";
+}
+
+console.log(theResult);
+
