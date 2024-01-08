fbf5872db5c32ad73a0f131734eaaa0bafdf1758
Prashant Kumar <pb28.rkt@gmail.com>
2024-01-08 22:14:53 +0530

    ComponentA

---
+++ 06.React_TypeScript/2-useContext/src/components/Context-API/ComponentA.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ComponentB from './ComponentB'
+// import { AppData } from './models/AppData'
+
+// interface IProps{
+//     appData : AppData
+// };
+
+export const ComponentA : React.FC= () => {
+
+    
+
+  return (
+    <>
+    <div className="container mt-3">
+        <div className="row">
+            <div className="col">
+                <div className="card">
+                    <div className="card-body bg-info">
+                        <p>Component A</p>
+                        {/* <ComponentB appData = {props.appData}/> */}
+                        <ComponentB/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ComponentA
